581ba332d755f3f302acea165e94559b4a7512e6
Ntdi <yogikngyt@gmail.com>
2022-07-03 11:47:43 +0200

    added high score

--- src/main.rs
+++ src/main.rs
@@ -3,36 +3,66 @@ use std::cmp::Ordering;
 use std::io;
 
 fn main() {
-    println!("Guess MY number");
-
-    let secret_number = rand::thread_rng().gen_range(1..=100);
-    let mut guess_amount = 0;
-
+    let mut high_score: i32 = 1000;
     loop {
-        println!("Enter Guess:");
+        println!("Guess MY number");
 
-        let mut guess = String::new();
+        let secret_number = rand::thread_rng().gen_range(1..=100);
+        let mut guess_amount = 0;
+        loop {
+            println!("Enter Guess:");
+
+            let mut guess = String::new();
+
+            io::stdin()
+                .read_line(&mut guess)
+                .expect("Unable to read");
+
+            let guess: u32 = match guess.trim().parse() {
+                Ok(num) => num,
+                Err(_) => {
+                    println!("Enter a number idiot");
+                    continue;
+                },
+            };
+
+            println!("You said: {}", guess);
+            guess_amount = guess_amount + 1;
+
+            match guess.cmp(&secret_number) {
+                Ordering::Less => println!("Too small lmao!"),
+                Ordering::Greater => println!("Too big (That's what she said)!"),
+                Ordering::Equal => {
+                    println!("\nWe have a WINNER!");
+                    println!("It took you {} guesses to guess the number.", guess_amount);
+                    match guess_amount.cmp(&mut high_score) {
+                        Ordering::Less => {
+                            println!("NEW HIGH-SCORE {}", guess_amount);
+                            high_score = guess_amount;
+                        }
+                        Ordering::Equal => {
+                            println!("You MATCHED your current HIGH-SCORE of {}", high_score)
+                        }
+                        Ordering::Greater => {
+                            println!("You current HIGH-SCORE is {}", high_score)
+                        }
+                    }
+                    break;
+                }
+            }
+        }
+        println!("\nWould you like to play again?\n(Y)es (N)o");
+        let mut response = String::new();
 
         io::stdin()
-            .read_line(&mut guess)
+            .read_line(&mut response)
             .expect("Unable to read");
 
-        let guess: u32 = match guess.trim().parse() {
-            Ok(num) => num,
-            Err(_) => continue,
-        };
-
-        println!("You said: {}", guess);
-        guess_amount = guess_amount + 1;
-
-        match guess.cmp(&secret_number) {
-            Ordering::Less => println!("Too small lmao!"),
-            Ordering::Greater => println!("Too big (That's what she said)!"),
-            Ordering::Equal => {
-                println!("We have a WINNER!");
-                println!("It took you {} guesses to guess the number.", guess_amount);
-                break;
-            }
+        if response.trim().to_lowercase() != "y" {
+            println!("See you next time");
+            break;
+        } else {
+            println!("Restarting game... \n");
         }
     }
 }
